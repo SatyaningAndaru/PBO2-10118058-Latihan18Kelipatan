0cbab57f622a9ed554c1697b30023cb7809cc275
SatyaningAndaru <55575075+SatyaningAndaru@users.noreply.github.com>
2019-10-09 18:00:55 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118058/latihan18kelipatan/PBO210118058Latihan18Kelipatan.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118058.latihan18kelipatan;
+
+/**
+ *
+ * @author user
+ * Nama : Satyaning Andaru Bawalaksana
+ * NIM : 10118058
+ * Kelas : IF-2
+ */
+public class PBO210118058Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Double i = 3.5;
+        
+        do{
+            System.out.println(i);
+            
+            i = i + 3.5;
+        } while(i<=39);
+        System.out.println();
+        System.out.println("Directed By Satyaning Andaru");
+    }
+    
+}
